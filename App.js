ddcc9c03f69a548c91a94a1fac05b04cef88c70a
ranakhaled1622 <142254217+ranakhaled1622@users.noreply.github.com>
2023-12-29 11:56:22 +0200

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+// src/Greetings.jsx
+import React from 'react';
+
+const Greetings = () => {
+  return (
+    <div>
+      <h1>Welcome to my first react Page</h1>
+      <p>how are you today</p>
+    </div>
+  );
+};
+
+export default Greetings;
---
+++ Greetings.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Greetings =() => {
+    return (
+        <div>
+            <h1> welcome to my first react page</h1>
+            <p>how are you today</p>
+        </div>
+    );
+    };
+    export default Greetings;
